5a57b84f799a111b509b72c4c305b191df9f26dd
Nghia Mai <jack@Jack.local>
2019-03-20 22:48:51 +0700

    apply stable version

---
+++ index.js
@@ -0,0 +1,55 @@
+const eosjs = require('eosjs');
+const express = require('express');
+
+const eos = eosjs({
+  httpEndpoint: 'http://kylin.fn.eosbixin.com',
+  chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191',
+  keyProvider: ['EOS7NkWM7qjSaT3czGKuAfdpaBJ6Bzri5V8Ksh24eQVQLKsvmHXZw'],
+  expireInSeconds: 60,
+  sign: true,
+});
+// eos.getInfo({}).then(res => console.log('res', res));
+eos
+  .transaction(
+    {
+      actions: [
+        {
+          account: 'trongdthdapp',
+          name: 'transfer',
+          authorization: [
+            {
+              actor: 'trongdthdapp',
+              permission: 'active',
+            },
+          ],
+          data: {
+            from: 'trongdthdapp',
+            to: 'tatsukakalot',
+            quantity: '1 CONST',
+            memo: '',
+          },
+        },
+      ],
+    },
+    // config -- example: {broadcast: false, sign: true}
+  )
+  .then(res => console.log('res', res))
+  .catch(e => console.error('HERE ERROR:', e));
+
+// const options = {
+//   authorization: 'trongdthdapp@active',
+//   broadcast: true,
+//   sign: true,
+// };
+
+// const PORT = 3000;
+// const app = express();
+// app.get('/', async (req, res) => {
+//   try {
+//     eos.transfer('trongdthdapp', 'tatsukakalot', '1.00 CONST', '', options);
+//   } catch (e) {
+//     console.log('\nCaught exception: ' + e);
+//     if (e instanceof RpcError) res.send(e.json);
+//   }
+// });
+// app.listen(PORT, () => console.log(`Example app listening on PORT ${PORT}!`));
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import 'dotenv/config';
-import express from 'express';
-import fetch from 'node-fetch';
-import { Api, JsonRpc, RpcError } from 'eosjs';
-import JsSignatureProvider from 'eosjs/dist/eosjs-jssig';
-import { TextEncoder, TextDecoder } from 'util';
+require('dotenv').config();
+const express = require('express');
+const fetch = require('node-fetch');
+const { Api, JsonRpc, RpcError } = require('eosjs');
+const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig');
+const { TextEncoder, TextDecoder } = require('util');
 
 const signatureProvider = new JsSignatureProvider([process.env.PRIVATE_KEY]);
 const rpc = new JsonRpc(process.env.NODE_ADDRESS, { fetch });
@@ -17,33 +17,33 @@ const PORT = 3000;
 const app = express();
 app.get('/', async (req, res) => {
   try {
-    // const resp = await rpc.get_account('tatsukakalot');
-    const resp = await api.transact(
-      {
-        actions: [
-          {
-            account: 'trongdthdapp',
-            name: 'transfer',
-            authorization: [
-              {
-                actor: 'trongdthdapp',
-                permission: 'active',
-              },
-            ],
-            data: {
-              from: 'trongdthdapp',
-              to: 'tatsukakalot',
-              quantity: '1 CONST',
-              memo: '',
-            },
-          },
-        ],
-      },
-      {
-        blocksBehind: 3,
-        expireSeconds: 30,
-      },
-    );
+    const resp = await rpc.get_account('tatsukakalot');
+    // const resp = await api.transact(
+    //   {
+    //     actions: [
+    //       {
+    //         account: 'trongdthdapp',
+    //         name: 'transfer',
+    //         authorization: [
+    //           {
+    //             actor: 'trongdthdapp',
+    //             permission: 'active',
+    //           },
+    //         ],
+    //         data: {
+    //           from: 'trongdthdapp',
+    //           to: 'tatsukakalot',
+    //           quantity: '1.00 CONST',
+    //           memo: '',
+    //         },
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     blocksBehind: 3,
+    //     expireSeconds: 30,
+    //   },
+    // );
     res.send(resp);
   } catch (e) {
     console.log('\nCaught exception: ' + e);
